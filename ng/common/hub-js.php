216507a9deb6a97dfa016177de84cbc738976f7a
cyleex@sbcglobal.net <cyleex@sbcglobal.net>
2018-09-17 15:19:47 -0700

    Update NG

--- ng/common/hub-js.php
+++ ng/common/hub-js.php
@@ -2,9 +2,10 @@
 <script src="/common/script/lib/jquery-latest.min.js"></script>
 <script src="/common/script/lib/jquery.timer.js"></script>    
 <script src="/bootstrap/bootstrap-3.3.5-dist/js/bootstrap.min.js"></script>
-<script src="/ng/common/script/angular.min.js"></script>
-<script src="/ng/common/script/angular-route.min.js"></script>
+<script src="/ng/common/script/angular.js"></script>
+<script src="/ng/common/script/angular-route.js"></script>
+<script src="/ng/common/script/angular-touch.js"></script>
 
 <!-- Additional JS Depenencies -->
-<script src="/ng/common/script/ngBoot.min.js"></script>
+<script src="/ng/common/script/ngBoot.js"></script>
 <script src="/common/script/checkform.js"></script>
